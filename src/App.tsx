324993b8ea93a74464ddbc80edc8c73baea5e502
Jonas Nilsson <nilssoncjonas@gmail.com>
2023-06-03 08:33:07 +0200

    added navigation, improved notfound, added dark bg/light color to body scss

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,27 @@
 import {Routes, Route} from 'react-router-dom'
+// Pages
 import Index from './pages/Index.tsx'
 import Search from './pages/Search.tsx'
 import NotFound from './pages/NotFound.tsx'
+// Components
+import Navigation from "./components/Navigation.tsx";
+// Style
+import Container from "react-bootstrap/Container";
 import './assets/scss/App.scss'
 
 const App = () => {
 
 	return (
 		<>
-			<Routes>
-				<Route path='/' element={<Index/>}/>
-				<Route path='/search' element={<Search/>}/>
-				<Route path='*' element={<NotFound/>}/>
-			</Routes>
+			<Navigation/>
+			<Container py-3>
+
+				<Routes>
+					<Route path='/' element={<Index/>}/>
+					<Route path='/search' element={<Search/>}/>
+					<Route path='*' element={<NotFound/>}/>
+				</Routes>
+			</Container>
 		</>
 	)
 }
--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,7 +1,17 @@
+import {Link} from "react-router-dom";
+// Style
+import {Button} from "react-bootstrap";
+
 const NotFound = () => {
 	return (
 		<>
 			<h1>404 | NotFound</h1>
+			<div>
+				<p>I Find Your Lack Of Navigation Disturbing</p>
+				<Link to="/" >
+					<Button variant="secondary">Help me, Obi-Wan Kenobi. You're my only hope.</Button>
+				</Link>
+			</div>
 		</>
 	)
 }
