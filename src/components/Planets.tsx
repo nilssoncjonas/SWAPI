cf55fdd5440f3d9ba68f10027defe2d35cb83896
Jonas Nilsson <nilssoncjonas@gmail.com>
2023-06-05 00:54:25 +0200

    planet component

---
+++ src/components/Planets.tsx
@@ -0,0 +1,23 @@
+import React from "react"
+import {Planet} from "../types/films";
+import ListGroup from "react-bootstrap/ListGroup";
+
+interface IProp {
+planets: Planet[]
+}
+
+const Planets:React.FC<IProp> = ({ planets }) => {
+	return (
+		<>
+			{planets.map((p, index) => (
+				<ListGroup.Item
+					action
+					href={''}
+					key={index}>
+					{p.name}
+				</ListGroup.Item>
+			))}
+		</>
+	)
+}
+export default Planets
